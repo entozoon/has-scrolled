467b6cde3ed2cbfe814d35cac1041deed0110cce
Michael Cook <michael.cook@thetechdept.com>
2019-09-25 11:40:39 +0100

    Could be a goer

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+const setHasScrolled = () => {
+  // OPTIMISATION - we could allow an option to have it run less frequently - frameskip style
+
+  let scroll =
+    document.documentElement && document.documentElement.scrollTop
+      ? document.documentElement.scrollTop
+      : document.body.scrollTop;
+
+  // Toggle a class if user has scrolled down somewhat
+  let somewhat = 50,
+    tolerance = 30;
+  // Allow a tolerance either way, for old pages as header is sticky, and it's a whole thing
+  if (scroll > somewhat + tolerance) {
+    document.body.classList.add("has-scrolled");
+  } else if (scroll < somewhat - tolerance) {
+    document.body.classList.remove("has-scrolled");
+  }
+
+  console.log(scrollTop);
+  window.requestAnimationFrame(setScrollParallax);
+};
 module.exports = () => {
-  window.addEventListener("scroll", () => {
-    let scrollTop =
-      (window.pageYOffset || document.documentElement.scrollTop) -
-      (document.documentElement.clientTop || 0);
-    console.log(scrollTop);
-  });
+  // Use animation frames rather than on scroll, as it's hard to say exactly when the page load position has locked in
+  window.requestAnimationFrame(setHasScrolled);
 };
