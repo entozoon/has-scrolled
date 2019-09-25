d3c776f41f66eae71962e812065f1935695a74bf
Michael Cook <michael.cook@thetechdept.com>
2019-09-25 12:16:48 +0100

    Scroll var is an instance, not constant

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const scrollY =
+export const scrollY = () =>
   document.documentElement && document.documentElement.scrollTop
     ? document.documentElement.scrollTop
     : document.body.scrollTop;
@@ -9,13 +9,13 @@ const setHasScrolled = () => {
     tolerance = 30;
 
   // Allow a tolerance either way, as for pages where the header is sticky it's a whole thing
-  if (scrollY > somewhat + tolerance) {
+  if (scrollY() > somewhat + tolerance) {
     document.body.classList.add("has-scrolled");
-  } else if (scrollY < somewhat - tolerance) {
+  } else if (scrollY() < somewhat - tolerance) {
     document.body.classList.remove("has-scrolled");
   }
   // <body data-scroll-y="120">
-  document.body.setAttribute("data-scroll-y", Math.floor(scrollY));
+  document.body.setAttribute("data-scroll-y", Math.floor(scrollY()));
 
   // OPTIMISATION - we could allow an option to have it run less frequently - frameskip style
   window.requestAnimationFrame(setHasScrolled);
