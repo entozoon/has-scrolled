54da82ac27866b15d33a4702570cd2cc65b1a3b2
Michael Cook <michael.cook@thetechdept.com>
2019-11-05 12:21:59 +0000

    Typescript compilation to ES2015, as we dont really want webpack imports to have to transpile

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-export const scrollY = () =>
-  document.documentElement && document.documentElement.scrollTop
-    ? document.documentElement.scrollTop
-    : document.body.scrollTop;
-
-const setHasScrolled = () => {
-  // Toggle a class if user has scrolled down somewhat. These should be parameters ideally
-  let somewhat = 50,
-    tolerance = 30;
-
-  // Allow a tolerance either way, as for pages where the header is sticky it's a whole thing
-  if (scrollY() > somewhat + tolerance) {
-    document.body.classList.add("has-scrolled");
-  } else if (scrollY() < somewhat - tolerance) {
-    document.body.classList.remove("has-scrolled");
-  }
-  // <body data-scroll-y="120">
-  document.body.setAttribute("data-scroll-y", Math.floor(scrollY()));
-
-  // OPTIMISATION - we could allow an option to have it run less frequently - frameskip style
-  window.requestAnimationFrame(setHasScrolled);
+"use strict";
+exports.__esModule = true;
+exports.scrollY = function () {
+    return document.documentElement && document.documentElement.scrollTop
+        ? document.documentElement.scrollTop
+        : document.body.scrollTop;
 };
-
-const hasScrolled = () => {
-  // Use animation frames rather than on scroll, as it's hard to say exactly when the page load position has locked in
-  window.requestAnimationFrame(setHasScrolled);
+var setHasScrolled = function () {
+    // Toggle a class if user has scrolled down somewhat. These should be parameters ideally
+    var somewhat = 50, tolerance = 30;
+    // Allow a tolerance either way, as for pages where the header is sticky it's a whole thing
+    if (exports.scrollY() > somewhat + tolerance) {
+        document.body.classList.add("has-scrolled");
+    }
+    else if (exports.scrollY() < somewhat - tolerance) {
+        document.body.classList.remove("has-scrolled");
+    }
+    // <body data-scroll-y="120">
+    document.body.setAttribute("data-scroll-y", String(Math.floor(exports.scrollY())));
+    // OPTIMISATION - we could allow an option to have it run less frequently - frameskip style
+    window.requestAnimationFrame(setHasScrolled);
 };
-export default hasScrolled;
+var hasScrolled = function () {
+    // Use animation frames rather than on scroll, as it's hard to say exactly when the page load position has locked in
+    window.requestAnimationFrame(setHasScrolled);
+};
+exports["default"] = hasScrolled;
---
+++ index.ts
@@ -0,0 +1,28 @@
+export const scrollY = () =>
+  document.documentElement && document.documentElement.scrollTop
+    ? document.documentElement.scrollTop
+    : document.body.scrollTop;
+
+const setHasScrolled = () => {
+  // Toggle a class if user has scrolled down somewhat. These should be parameters ideally
+  let somewhat = 50,
+    tolerance = 30;
+
+  // Allow a tolerance either way, as for pages where the header is sticky it's a whole thing
+  if (scrollY() > somewhat + tolerance) {
+    document.body.classList.add("has-scrolled");
+  } else if (scrollY() < somewhat - tolerance) {
+    document.body.classList.remove("has-scrolled");
+  }
+  // <body data-scroll-y="120">
+  document.body.setAttribute("data-scroll-y", String(Math.floor(scrollY())));
+
+  // OPTIMISATION - we could allow an option to have it run less frequently - frameskip style
+  window.requestAnimationFrame(setHasScrolled);
+};
+
+const hasScrolled = () => {
+  // Use animation frames rather than on scroll, as it's hard to say exactly when the page load position has locked in
+  window.requestAnimationFrame(setHasScrolled);
+};
+export default hasScrolled;
